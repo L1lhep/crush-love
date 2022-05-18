f49b9154ffdd80e0288848beec15bfe81d019e8a
L1lhep <105789827+L1lhep@users.noreply.github.com>
2022-05-18 20:14:44 +0800

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,9 +1,9 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "周密迦 I Love You",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
+    "你愿意做我的女朋友嘛？ ^^",
+  buttonYes: "我愿意",
+  buttonNo: "对不起，我们不合适",
   titleModar: "Cảm ơn em vì đồng ý",
   descriptionModar:
     'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
